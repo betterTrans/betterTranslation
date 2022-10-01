e60abed7170a9c7bb35165e3257ed27fe9bb4935
betterTrans <bt.bettertrans@gmail.com>
2022-10-01 19:04:56 +0800

    1. 用 chrome.storage.local 來記錄 API_KEY，捨棄【背景服務】的全域變數做法 2. popup 直接可以存取 chrome.storage，不必透過 message 機制與【背景服務】溝通 3. popup 改在 DOMContentLoaded 之後才執行相關程式碼 4. 彈出頁面顯示 API_KEY 的程式碼獨立成函式，減少重複程式碼，讓程式碼更簡潔。 5. 索取快速鍵說明的程式碼獨立成函式，後續再改進。

--- background/background.js
+++ background/background.js
@@ -1,38 +1,33 @@
-// 全域變數
-var api_key = null    // 設為全域變數，稍後向外部請求翻譯時還會用到。
-chrome.storage.local.get('api_key', r => {
-    api_key = r.api_key
-});
 
 // 回應【網頁內容】或【彈出頁面】送過來的請求
 chrome.runtime.onMessage.addListener( (message, sender, sendResponse) => {
-    if (message.cmd == 'api_key') {
-        api_key = message.data.api_key
-        chrome.storage.local.set({api_key: api_key})
-    }
-    else if (message.cmd == 'remove_api_key') {
-        api_key = null
-        chrome.storage.local.remove('api_key')
-    }
-    else if (message.cmd == 'get_google_translation_V2') {
+    if (message.cmd == 'get_google_translation_V2') {
         var sent_html = message.data.sent_html
-
-        if (api_key) {
-            getGoogleTranslationV2(api_key, sent_html)
-            .then((tran_sent_html)=> {
-                // 取得譯文之後...
-                // 再次運用 sendMessage 機制，把譯文送回去。
-                // 不過這次不需要再用 chrome.tabs.query()，
-                // 因為我們從 sender 就知道請求是誰送過來的，翻譯結果當然要送回去給它才對。
-                chrome.tabs.sendMessage(
-                    sender.tab.id,
-                    {
-                        cmd: 'tran_sent_html',
-                        data: {tran_sent_html: tran_sent_html}
-                    }
-                );
-            })
-        }
+        
+        chrome.storage.local.get('api_key', r => {
+            if (r.api_key) {
+                // 注意！Promise 裡的 Promise。。。Promise chain...
+                return getGoogleTranslationV2(r.api_key, sent_html)
+                .then((tran_sent_html)=> {
+                    // 取得譯文之後...
+                    // 再次運用 sendMessage 機制，把譯文送回去。
+                    // 不過這次不需要再用 chrome.tabs.query()，
+                    // 因為我們從 sender 就知道請求是誰送過來的，翻譯結果當然要送回去給它才對。
+                    chrome.tabs.sendMessage(
+                        sender.tab.id,
+                        {
+                            cmd: 'tran_sent_html',
+                            data: {tran_sent_html: tran_sent_html}
+                        }
+                    );
+                })
+            }
+            else {
+                msg = '找不到 API_KEY...請先設定好您的 API_KEY！'
+                console.log(msg)
+                sendResponse(msg)
+            }
+        });
     }
 });
 
--- popup/popup.js
+++ popup/popup.js
@@ -1,51 +1,54 @@
-chrome.storage.local.get('api_key', r => {
-    api_key = r.api_key
-    if (api_key) {
-        var partial_api_key = (api_key.length>5)? api_key.substring(0,5)+'......' : api_key
-        document.querySelector("#current_api_key").innerHTML = `目前 API_KEY：${partial_api_key}`
+window.addEventListener('DOMContentLoaded', ()=> {
+
+    // 向 content-script 索取快速鍵相關資訊
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {cmd: 'get_hotkeys_desc', data: {}}, (hotkeys_desc) => {
+            showHotkeyDescOnPopup(hotkeys_desc)
+        });
+    });
+
+    // 在彈出頁面上顯示 API_KEY 資訊
+    showAPI_KEYOnPopup()
+
+    // 點擊【送出】按鍵的話...
+    document.querySelector("#api_key_confirm_button").onclick = e =>{
+        var api_key = document.querySelector("#api_key_input_text").value
+        chrome.storage.local.set({api_key: api_key}, showAPI_KEYOnPopup);
+    }
+
+    // 在輸入框按下【Enter】的話...
+    document.querySelector("#api_key_input_text").onkeydown = e => {
+        if (e.key == 'Enter') {
+            var api_key = e.target.value
+            chrome.storage.local.set({api_key: api_key}, showAPI_KEYOnPopup);
+        }
+    }
+
+    // 點擊【清除】按鍵的話...
+    document.querySelector("#api_key_clear_button").onclick = e =>{
+        chrome.storage.local.remove('api_key', showAPI_KEYOnPopup);
     }
 })
 
-
-// 發出 message，向 content-script 索取資訊
-chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, {cmd: 'get_hotkeys_desc', data: {}}, (hotkeys_desc) => {
-        // 從 content-script 那邊收到回應。。。
-        if (hotkeys_desc) {
-            var ul=document.querySelector('ul#hotkey_desc')
-            ul.innerHTML = ''   // 先清除原來的內容。。。
-            hotkeys_desc.forEach(item=>{
-                ul.innerHTML += "<li>" + item + "</li>"
-            })
-        }
-    });
-});
-
-// 點擊【送出】按鍵的話...
-document.querySelector("#api_key_confirm_button").onclick = e =>{
-    var api_key = document.querySelector("#api_key_input_text").value
-    sendAPIKEY2Background(api_key)
-}
-
-// 在輸入框按下【Enter】的話...
-document.querySelector("#api_key_input_text").onkeydown = e => {
-    if (e.key == 'Enter') {
-        var api_key = e.target.value
-        sendAPIKEY2Background(api_key)
+function showHotkeyDescOnPopup(hotkeys_desc) {
+    if (hotkeys_desc) {
+        var ul=document.querySelector('ul#hotkey_desc')
+        ul.innerHTML = ''   // 先清除原來的內容。。。
+        hotkeys_desc.forEach(item=>{
+            ul.innerHTML += "<li>" + item + "</li>"
+        })
     }
 }
 
-// 點擊【清除】按鍵的話...
-document.querySelector("#api_key_clear_button").onclick = e =>{
-    chrome.storage.local.remove('api_key',()=>{
-        document.querySelector("#current_api_key").innerHTML = '目前尚未設定 API_KEY。。。'
-    });
-    chrome.runtime.sendMessage({cmd: 'remove_api_key'});
-}
-
-// 把 API_KEY 送往背景服務
-function sendAPIKEY2Background(api_key) {
-    var partial_api_key = (api_key.length>5)? api_key.substring(0,5)+'......' : api_key
-    document.querySelector("#current_api_key").innerHTML = `目前 API_KEY：${partial_api_key}`
-    chrome.runtime.sendMessage({cmd: 'api_key', data: {api_key: api_key}});
+function showAPI_KEYOnPopup() {
+    chrome.storage.local.get('api_key', r => {
+        var div = document.querySelector("#current_api_key")
+        if (r.api_key) {
+            var partial_api_key = (r.api_key.length>5)? r.api_key.substring(0,5)+'......' : r.api_key
+            div.innerHTML = `目前 API_KEY：${partial_api_key}`
+        }
+        else {
+            div.innerHTML = '目前尚未設定 API_KEY。。。'
+        }
+    })
 }
